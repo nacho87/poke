db39d8f46d14e399d40bc46a5156933e3838eff0
Nacho Decima <jidecima87@gmail.com>
2022-09-01 19:11:53 -0300

    Implement first iteration for infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+import './css/base.css'
+import SvgSprites from './components/SvgSprites'
+import Header from './components/Header/Header'
+import Search from './components/Search/Search'
+import PokeCard from './components/PokeCard/PokeCard'
+
 
 function App() {
+  const [data, setData] = useState([]);
+
+  let next;
+
+ 
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then(response => response.json())
+      .then(data => {
+        next = data.next;
+        setData(data.results)
+      });
+  },[])
+
+  function obCallback(payload) {
+    console.log('nacho', payload[0].intersectionRatio);
+    if (payload[0].intersectionRatio > 0) {
+      console.log('next', next)
+      fetch(next)
+        .then(response => response.json())
+        .then(newData => {
+          next = newData.next;
+          const localData = newData.results;
+          const newArrayData = localData.map(item => data.push(item))
+          console.log('newArrayData', newArrayData)
+          setData(data.concat(localData))
+        });
+    }
+  }
+
+  useEffect(() => {
+
+    const watch = document.querySelector('.loadMore');
+    const ob = new IntersectionObserver(obCallback, watch);
+    ob.observe(watch);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SvgSprites/>
+      <Header/>
+      <Search/>
+      { 
+        data?.length > 0 && data?.map(item => {
+          return <PokeCard key={item.name} name={item.name} url={item.url}/>
+        })
+      }
+      <div className='loadMore'>LoadMore</div>
     </div>
   );
 }
--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 import poke1 from '../../assets/generations/generation1/001.png';
@@ -6,24 +6,37 @@ import poke1 from '../../assets/generations/generation1/001.png';
 import './poke-card.css';
 
 
-export default function PokeCard() {
-  return(
-    <div className="poke-card" style={{'background': `var(--type-bg-grass)`}}>
-      <div className="poke-card-container">
-        <svg className='poke-card-pat6'>
-          <use xlinkHref='#pat-6'></use>
-        </svg>
-        <svg className='poke-card-pokeball'>
-          <use xlinkHref='#pat-pokeball'></use>
-        </svg>
+export default function PokeCard({name, url}) {
+  const [data, setData] = useState({})
+
+  useEffect(()=>{
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setData(data))
+  },[])
+
+  if (data.name) {
+    return(
+      <div className="poke-card" style={{'background': `var(--type-bg-${data.types[0].type.name})`}}>
+        <div className="poke-card-container">
+          <svg className='poke-card-pat6'>
+            <use xlinkHref='#pat-6'></use>
+          </svg>
+          <svg className='poke-card-pokeball'>
+            <use xlinkHref='#pat-pokeball'></use>
+          </svg>
+        </div>
+        <div className="poke-number">#{data.order.toString().padStart(3, '0')}</div>
+        <div className="poke-name">{name}</div>
+        <div className="poke-labels">
+          {
+            data.types.map(value => <Button key={value.type.name} label={value.type.name}/>)
+          }
+        </div>
+        <img className="poke-img" src={data?.sprites?.front_default} alt={name}/>
       </div>
-      <div className="poke-number">#001</div>
-      <div className="poke-name">Bullbasaur</div>
-      <div className="poke-labels">
-        <Button label="grass"/>
-        <Button label="poison"/>
-      </div>
-      <img className="poke-img" src={poke1} />
-    </div>
-  );
+    );
+  }
+
+  
 }
